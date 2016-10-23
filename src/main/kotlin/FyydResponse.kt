264bd3c58f49271d9bc477b842adccdbb5e066de
Martin Fietz <Martin.Fietz@gmail.com>
2016-10-23 16:26:08 +0200

    Add response models

---
+++ src/main/kotlin/FyydResponse.kt
@@ -0,0 +1,25 @@
+package de.mfietz.fyydlin
+
+import com.squareup.moshi.Json
+import java.util.*
+
+data class FyydResponse(
+    val status: Int,
+    val msg: String,
+    val data: Map<Long, SearchHit>
+)
+
+data class SearchHit(
+    val title: String,
+    val slug: String,
+    val language: String,
+    @Json(name = "xml_url") val xmlUrl: String,
+    @Json(name = "html_url")val htmlUrl: String,
+    @Json(name = "img_url") val imageUrl: String,
+    val description: String,
+    val subtitle: String,
+    val categories: List<List<String>>,
+    val author: String,
+    @Json(name = "lastpubdate") val lastPubDate: Date,
+    @Json(name = "count_episodes") val countEpisodes: String
+)
