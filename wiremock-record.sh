857dff97e6ee3b4d3f3100fbe6a908712bb4cdf3
Martin Fietz <Martin.Fietz@gmail.com>
2016-10-23 21:41:19 +0200

    Start and stop wiremock correctly

--- wiremock-record.sh
+++ wiremock-record.sh
@@ -5,12 +5,16 @@ if [[ ! -f wiremock-standalone-2.2.2.jar ]] ; then
     exit
 fi
 
-java -jar wiremock-standalone-2.2.2.jar --proxy-all="https://api.fyyd.de" --record-mappings --verbose
+java -jar wiremock-standalone-2.2.2.jar --proxy-all="https://api.fyyd.de" --record-mappings --verbose &
+
+sleep 5
 
 curl "http://localhost:8080/search-pdc/wrint"
 curl "http://localhost:8080/search-pdc/wrint/5"
 curl "http://localhost:8080/search-pdc/wrint,freakshow"
 curl "http://localhost:8080/search-pdc/wrint,freakshow/5"
+
+kill $!
 
 rm -rf src/test/resources/__files
 rm -rf src/test/resources/mappings
