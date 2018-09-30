915c6bcb9e3c21772ab1ea70cf2e167b07d8e14e
Martin Fietz <martin.fietz@gmail.com>
2018-09-30 20:19:56 +0200

    Fix path

--- src/main/kotlin/FyydService.kt
+++ src/main/kotlin/FyydService.kt
@@ -6,7 +6,7 @@ import retrofit2.http.Query
 
 interface FyydService {
 
-    @GET("/search/podcast")
+    @GET("search/podcast")
     fun searchPodcasts(@Query("title") title: String,
                        @Query("count") limit: Int?): Single<FyydResponse>
 
